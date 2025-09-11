57dd4e1dacd34a371b6cbe89cb3603f2e39ce3f6
andyjianzhou <andyzhou727@gmail.com>
2025-09-10 19:21:54 -0600

    Fixed small bug

--- code/app/src/main/java/com/example/listcity/MainActivity.java
+++ code/app/src/main/java/com/example/listcity/MainActivity.java
@@ -69,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
             bottomSheetDialog.show();
+            selectedCityPos = -1;
         });
 
         deleteButton.setOnClickListener(v -> {
